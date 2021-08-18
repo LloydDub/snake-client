c06deadcbd1923c2b100aeaa7579c9288fe4ee45
Lloyd Worth <lloydrww@gmail.com>
2021-08-18 22:27:15 +0000

    setup stdin input to close

--- play.js
+++ play.js
@@ -3,3 +3,25 @@ const  connect  = require('./client');
 console.log('Connecting ...');
 
 connect();
+
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+setupInput()
+
+const handleUserInput = function() {
+  process.stdin.on('data', (data) => {
+    if (data === '\u0003') {
+      process.exit();
+    }
+  });
+}
+
+
+handleUserInput()

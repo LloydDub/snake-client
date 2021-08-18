cf80a0024130811f153f4c89f4ccc99285b36c11
Lloyd Worth <lloydrww@gmail.com>
2021-08-18 20:31:57 +0000

    added player intials

--- client.js
+++ client.js
@@ -2,24 +2,34 @@
  * Establishes connection with the game server
  */
 
- const net = require('net');
+const net = require('net');
 
  
- const connect = function() {
+const connect = function() {
   const conn = net.createConnection({ 
     host: 'localhost',
     port: 50541
   });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+
+  conn.on('connect', () => {
+    conn.write('Name: LRW');
+  });
+  conn.setEncoding('utf8'); // interpret incoming data as text
+
+  conn.on('connect', () => {
+    console.log("Connection Established");
+  })
+
+  
+
   conn.on('data', (data) => {
-    console.log('Server says: ', data);
+    console.log('Server says:', data);
   });
   return conn;
 }
 
 
-connect();
+
 
 
 

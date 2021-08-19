c9b5ffe1b8fdba2a8b15193169b07812881162b4
Lloyd Worth <lloydrww@gmail.com>
2021-08-19 19:17:22 +0000

    added canned messages

--- input.js
+++ input.js
@@ -17,7 +17,7 @@ const handleUserInput = function(key) {
     }
 
     if (key === '\u0031') {
-      connection.write("Say: Nice")
+      connection.write("Say: Nice, I mean hiss")
     } if (key === '\u0032') {
       connection.write("Say: You got me")
     } if (key === '\u0033') {

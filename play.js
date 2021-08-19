b7152a6184970e60bb5112ade14af96228e89116
Lloyd Worth <lloydrww@gmail.com>
2021-08-19 01:48:07 +0000

    refactored and moved input module

--- play.js
+++ play.js
@@ -1,27 +1,31 @@
 const net = require('net');
 const  connect  = require('./client');
+const { setupInput } = require('./input');
+
+
+
 console.log('Connecting ...');
 
 connect();
 
 
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-}
-setupInput()
+// const setupInput = function() {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding('utf8');
+//   stdin.resume();
+//   return stdin;
+// }
+// setupInput()
 
-const handleUserInput = function() {
-  process.stdin.on('data', (data) => {
-    if (data === '\u0003') {
-      process.exit();
-    }
-  });
-}
+// const handleUserInput = function() {
+//   process.stdin.on('data', (data) => {
+//     if (data === '\u0003') {
+//       process.exit();
+//     }
+//   });
+// }
 
 
-handleUserInput()
+// handleUserInput()

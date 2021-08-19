53df974cbcd9d771b8f12b93fceb6100e8f119f3
Lloyd Worth <lloydrww@gmail.com>
2021-08-19 18:20:26 +0000

    added movement controls

--- input.js
+++ input.js
@@ -1,21 +1,35 @@
-const setupInput = function() {
+let connection;
+
+
+
+const handleUserInput = function(key) {
+  
+    if (key === '\u0003') {
+      process.exit();
+    } if (key === '\u0077') {
+      connection.write("Move: up")
+    } if (key === '\u0061') {
+      connection.write("Move: left")
+    } if (key === '\u0064') {
+      connection.write("Move: right")
+    } if (key === '\u0073') {
+      connection.write("Move: down")
+    } 
+  
+}
+
+
+
+const setupInput = function(conn) {
   const stdin = process.stdin;
+  connection = conn;
   stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
+  stdin.setEncoding('utf8');  
+  stdin.resume(); 
+  stdin.on('data', key => handleUserInput(key));
   return stdin;
 }
-setupInput()
-
-const handleUserInput = function() {
-  process.stdin.on('data', (data) => {
-    if (data === '\u0003') {
-      process.exit();
-    }
-  });
-}
 
 
-handleUserInput()
 
 module.exports = { setupInput }

d2730ec3d007b40a1ce64b94dfac7008ed43b492
Lloyd Worth <lloydrww@gmail.com>
2021-08-22 03:42:52 +0000

    removed code from when I was figuring it out cleaned up

--- play.js
+++ play.js
@@ -1,30 +1,12 @@
 const net = require('net');
+
 const  { connect }  = require('./client');
+
 const { setupInput } = require('./input');
-
-
 
 console.log('Connecting ...');
 
 
 
-setupInput(connect());
-
-// const setupInput = function() {
-//   const stdin = process.stdin;
-//   stdin.setRawMode(true);
-//   stdin.setEncoding('utf8');
-//   stdin.resume();
-//   return stdin;
-// }
-// setupInput()
-
-// const handleUserInput = function() {
-//   process.stdin.on('data', (data) => {
-//     if (data === '\u0003') {
-//       process.exit();
-//     }
-//   });
-// }
 
 

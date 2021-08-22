41fdf10cd5873c13d884ba39fafdf2e0d741e23a
Lloyd Worth <lloydrww@gmail.com>
2021-08-22 03:49:42 +0000

    added comments per project instructions

--- client.js
+++ client.js
@@ -7,15 +7,15 @@ const connect = function() {
     host: IP, 
     port: PORT
   });
-  conn.setEncoding('utf8');
+  conn.setEncoding('utf8'); //incoming data is encoded as text
  
- conn.on('connect', () => {
+ conn.on('connect', () => {  // adds the player initals to snake
     console.log("Connection Established");
     conn.write('Name: LRW');    
   });
   
   conn.on('data', (data) => {
-    console.log('Server says:', data);
+    console.log('Server says:', data);   // return msg from server if player idles to long
   });
   return conn;
 };

d5563ce4493649bc25d4dbb481e5775e0aa784cd
Lloyd Worth <lloydrww@gmail.com>
2021-08-22 03:40:57 +0000

    Refactored and removed notes and comments

--- client.js
+++ client.js
@@ -1,11 +1,3 @@
-/**
- * Establishes connection with the game server
- */
- 
-//  setInterval(function() { callback( "  params"); }, 50); 
-
-
-
 const net = require('net');
 
 const { IP, PORT,  } = require('./constants');
@@ -15,25 +7,17 @@ const connect = function() {
     host: IP, 
     port: PORT
   });
-  conn.setEncoding('utf8'); // interpret incoming data as text
-
+  conn.setEncoding('utf8');
  
  conn.on('connect', () => {
     console.log("Connection Established");
     conn.write('Name: LRW');    
-    // setInterval(function() {
-    //   conn.write("Move: up");
-    // }, 50)
-  })
-  
+  });
   
   conn.on('data', (data) => {
     console.log('Server says:', data);
   });
   return conn;
-}
-
-
-
+};
 
 module.exports =  { connect };
